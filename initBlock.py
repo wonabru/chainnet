bd55863327f9938d7a0d5f4b8873cf1f185d40e8
Krzysztof Urbanowicz <wonabru@gmail.com>
2019-04-30 13:57:49 +0200

    i

--- initBlock.py
+++ initBlock.py
@@ -18,11 +18,9 @@ class CInitBlock(CGenesis):
             self.baseTotalSupply = 23000000.23
             self.initAccount = CBaseLimitedToken(self.kade, 'Q', self.baseTotalSupply, address=self.initAccountPubKey)
             self.initAccount.amount[self.initAccount.address] = self.baseTotalSupply
-            self.firstAccount = CBaseAccount(self.kade, 'wonabru', address='1')
+            self.firstAccount = CBaseAccount(self.kade, 'wonabru', address=self.first_accountPubKey)
             #self.wallet_first = CWallet('wonabru')
-            self.firstAccount.address = self.first_accountPubKey # self.wallet_first.pubKey #
-            #TODO
-            #self.initAccount.chain.uniqueAccounts[self.firstAccount.address] = self.firstAccount
+            self.initAccount.chain.uniqueAccounts[self.firstAccount.address] = self.firstAccount
             self.firstAccount.chain.uniqueAccounts[self.initAccount.address] = self.initAccount
             self.initAtomicTransaction = CAtomicTransaction(self.initAccount, self.firstAccount, self.baseTotalSupply, optData='initTransaction', token=self.initAccount, time='2019-04-28 17:00:00')
             _hash = self.initAtomicTransaction.getHash()
