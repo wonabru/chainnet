d03303514a99653acbed8f67132f7f24c814d0a1
Krzysztof Urbanowicz <krzysztof.urbanowicz@edulab.io>
2019-05-03 04:13:16 +0200

    pecial char

--- isolated_functions.py
+++ isolated_functions.py
@@ -1,7 +1,20 @@
 import ast
+import re
 
 def str2obj(s):
     return ast.literal_eval(s.replace('true', 'True').replace('false', 'False'))
+
+
+def remove_special_char(in_seq):
+	"""
+	Function is responsible for normalize strings to defined format (UPPERCASE with '_' replacing any special character)
+	:param in_seq: list of strings
+	:return: list of strings
+	"""
+	_sub = re.sub(" {1,3}", "_", in_seq.strip()).upper()
+	_chars = ['*', '\\', '&', '/']
+	for x in _chars: _sub = _sub.replace(x, '_')
+	return _sub
 
 
 class CFinish:
